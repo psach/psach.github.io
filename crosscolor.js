0513a283b696ec2a02a2e677fc92698dea39de92
psach <37810543+psach@users.noreply.github.com>
2018-04-24 22:34:41 +0530

    Update crosscolor.js

--- crosscolor.js
+++ crosscolor.js
@@ -261,7 +261,7 @@ function popWords(words){
 				var gridClone = $("#cwd-grid").clone(true);
 				$("#cwd-grid").remove();
 				
-				gridClone.appendTo(gridParent).fadeOut('slow').fadeIn('slow');
+				gridClone.appendTo(gridParent).css('display','none').fadeIn('slow');
 				
 	
 			
