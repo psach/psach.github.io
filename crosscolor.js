6b019f8ec30f16ac90de0ebb7dd8f490ee0f3c9d
psach <praveensacharya@gmail.com>
2018-04-25 11:58:27 +0530

    a

--- crosscolor.js
+++ crosscolor.js
@@ -262,7 +262,7 @@ function popWords(words){
 				//gridParent.css('visibility','hidden');
 				var gridClone = $("#cwd-grid").clone(true);
 				$("#cwd-grid").remove();
-				gridClone.appendTo(gridParent);
+				gridClone.appendTo(gridParent).fadeIn(2000);
 				if(level>0){
 					gridClone.fadeOut(2000,function(){
 						
