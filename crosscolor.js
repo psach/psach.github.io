c7c62abb5577a12406683214572db5bc4a8b6195
psach <praveensacharya@gmail.com>
2018-04-25 17:01:13 +0530

    a

--- crosscolor.js
+++ crosscolor.js
@@ -166,8 +166,8 @@ $("#words").find(".cwd-tile-letter").click(function() {
 					
 					start = $("[row="+startCell[currLevel][0]+"][col="+startCell[currLevel][1]+"]");
 					end = $("[row="+endCell[currLevel][0]+"][col="+endCell[currLevel][1]+"]");
-					start.removeClass('strikeout '+greenChar);
-					end.removeClass('strikeout '+redChar);
+					start.find('.cwd-tile-letter').removeClass('strikeout '+greenChar);
+					end.find('.cwd-tile-letter').removeClass('strikeout '+redChar);
 					
 					/*
 					start.addClass("d3 green");
