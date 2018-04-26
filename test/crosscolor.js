48d6aa88527753bf5aa67d09905805ad8085760b
psach <37810543+psach@users.noreply.github.com>
2018-04-26 23:10:20 +0530

    Update crosscolor.js

--- test/crosscolor.js
+++ test/crosscolor.js
@@ -31,7 +31,7 @@ function popWords(words){
 						});
 						
 			$("#words").find(".cwd-tile-letter").click(function() {
-				alert($("#cwd-grid").hasClass('cwd-tile-highlight'));
+				alert(activeSet.hasClass('cwd-tile-highlight'));
 				if(!$("#cwd-grid").hasClass('cwd-tile-highlight')) return;
 	
 				var word = $(this).attr('word');
