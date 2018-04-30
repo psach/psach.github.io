075a7eae67c62a8817222e00e5d06d62442ecedb
psach <37810543+psach@users.noreply.github.com>
2018-04-30 15:40:01 +0530

    Update crosscolor.js

--- test/crosscolor.js
+++ test/crosscolor.js
@@ -337,9 +337,9 @@ function popWords(words){
 				//gridParent.css('visibility','hidden');
 				var gridClone = $("#cwd-grid").clone(true);
 				$("#cwd-grid").remove();
-				//gridClone.css('opacity', '1');
-				gridClone.appendTo(gridParent);
 				
+				gridClone.appendTo(gridParent);
+				gridParent.css('opacity', '1');
 				//gridClone.css('transition','opacity 2s ease-in-out');
 				/*gridClone.appendTo(gridParent).fadeIn(2000,function(){
 						start = $("[row="+startCell[lvl][0]+"][col="+startCell[lvl][1]+"]");
