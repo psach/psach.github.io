c42c485810e3059cfd5ebfbc8de7d3d1bb0f249f
psach <37810543+psach@users.noreply.github.com>
2018-05-02 12:14:19 +0530

    Update levelDataFruit.js

--- test/levelDataFruit.js
+++ test/levelDataFruit.js
@@ -5,8 +5,8 @@
 			var levelEndCell=[];
 			
 			var emojiChar = {"A":'\ud83c\udf4a',"B":'\ud83c\udf4c',"C":'\ud83c\udf52',"D":'\ud83c\udf46',
-							 "E":'\ud83e\udf55',"F":'\ud83c\udf6c',"G":'\ud83c\udf47',"H":'\ud83c\udf69',
-							 "I":'\ud83e\udd55',"J":'\ud83c\udf66',"K":'\ud83c\udf4f',"L":'\ud83c\udf4b',
+							 "E":'\ud83c\udf82',"F":'\ud83c\udf6c',"G":'\ud83c\udf47',"H":'\ud83c\udf69',
+							 "I":'\ud83c\udf6d',"J":'\ud83c\udf66',"K":'\ud83c\udf4f',"L":'\ud83c\udf4b',
 							 "M":'\ud83e\udd54',"N":'\ud83c\udf3d',"O":'\ud83c\udf51',"P":'\ud83c\udf4d',
 							 "Q":'\ud83e\udf50',"R":'\ud83c\udf4e',"S":'\ud83c\udf53',"T":'\ud83c\udf45',
 							 "U":'\ud83e\udd5d',"V":'\ud83c\udf6b',"W":'\ud83c\udf70',"X":'\ud83c\udf5a',
