d9152d0e9bb2a20f49187010b24bb33fe9f558fc
psach <37810543+psach@users.noreply.github.com>
2018-05-02 12:20:05 +0530

    Update levelDataFruit.js

--- test/levelDataFruit.js
+++ test/levelDataFruit.js
@@ -7,9 +7,9 @@
 			var emojiChar = {"A":'\ud83c\udf4a',"B":'\ud83c\udf4c',"C":'\ud83c\udf52',"D":'\ud83c\udf46',
 							 "E":'\ud83c\udf82',"F":'\ud83c\udf6c',"G":'\ud83c\udf47',"H":'\ud83c\udf69',
 							 "I":'\ud83c\udf6d',"J":'\ud83c\udf66',"K":'\ud83c\udf4f',"L":'\ud83c\udf4b',
-							 "M":'\ud83e\udd54',"N":'\ud83c\udf3d',"O":'\ud83c\udf51',"P":'\ud83c\udf4d',
-							 "Q":'\ud83e\udf50',"R":'\ud83c\udf4e',"S":'\ud83c\udf53',"T":'\ud83c\udf45',
-							 "U":'\ud83e\udd5d',"V":'\ud83c\udf6b',"W":'\ud83c\udf70',"X":'\ud83c\udf5a',
+							 "M":'\ud83c\udf44',"N":'\ud83c\udf3d',"O":'\ud83c\udf51',"P":'\ud83c\udf4d',
+							 "Q":'\ud83c\udf6f',"R":'\ud83c\udf4e',"S":'\ud83c\udf53',"T":'\ud83c\udf45',
+							 "U":'\ud83c\udf5f',"V":'\ud83c\udf6b',"W":'\ud83c\udf70',"X":'\ud83c\udf5a',
 							 "Y":'\ud83c\udf60',"Z":'\ud83c\udf61'};
 
 			var correctAns =[["RAGA","RANGE"/*,"APPLEPIE","LITTLEMAN","EXAM"*/],["DIAL","EIGHT"/*,"ALPHABET","ALCOHOLIC","EXAM"*/]];
