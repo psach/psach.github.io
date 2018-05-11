daa052942f123579f7d9af6bb6cc969305c2535a
psach <praveensacharya@gmail.com>
2018-05-11 19:09:43 +0530

    a

--- test/crosscolor.js
+++ test/crosscolor.js
@@ -640,9 +640,16 @@ function help(){
 	setTimeout(function(){$('[downclueid="18"]').removeClass('cwd-tile-highlight');},11000);
 		
 	var offset=$('.word-container').offset();
-	var startHelp = $('<div class="bouncedown"> Find crossword path from start to end <br/> using below 👇 words. For next set of word use right/left arrows</div>');
+	var startHelp = $('<div class="wrapperHelpR"> Find crossword path from start to end <br/> using below 👇 words. Use right/left arrows <br/> for next set of words</div>');
 	//alert(offset);
-	startHelp.attr('style','position:absolute;opacity:0;text-shadow:none;left:'+(offset.left+50)+'px; top:'+(offset.top-10)+'px; background-color:#DDFF96;  border-radius:3px;border: solid 0.05em dimgray; -webkit-animation-delay:11s;');
+	startHelp.attr('style','position:absolute;opacity:1;text-shadow:none;left:'+(offset.left+30)+'px; top:'+(offset.top-40)+'px; background-color:#DDFF96;  border-radius:3px;border: solid 0.05em dimgray; -webkit-animation-delay:11s;');
 	$('.centerbody').append(startHelp);
+	setTimeout(function(){
+		
+		$('.bouncesideright').attr('style','visibility:hidden;display:none');
+		$('.wrapperHelpR').attr('style','visibility:hidden;display:none');
+		$('.bounceside').attr('style','visibility:hidden;display:none');
+		
+	},20000);
 	
 }
