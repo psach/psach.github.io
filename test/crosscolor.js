dfc2c9a7f99d82553580e6d66d2243a4dc6c578d
psach <praveensacharya@gmail.com>
2018-05-12 17:08:26 +0530

    a

--- test/crosscolor.js
+++ test/crosscolor.js
@@ -658,7 +658,7 @@ function help(){
 		var offset=$('.arrow:eq(1)').offset();
 		var startHelp = $('<div class="wrapperHelpL"> Use right/left arrows 👉 </br/> to get next set of words  </div>');
 		//alert(offset);
-		startHelp.attr('style','position:absolute;width:165px;opacity:1;text-shadow:none;left:'+
+		startHelp.attr('style','position:absolute;width:170px;opacity:1;text-shadow:none;left:'+
 				   (offset.left-165)+'px; top:'+(offset.top-5)+
 				   'px; background-color:#DDFF96;  border-radius:3px;border: solid 0.05em dimgray; -webkit-animation-delay:18s;');
 		$('.centerbody').append(startHelp);
