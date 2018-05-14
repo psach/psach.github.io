4b5b370583b016496d7e88248d32049f63b9d890
psach <37810543+psach@users.noreply.github.com>
2018-05-14 12:29:25 +0530

    Update crosscolor.js

--- test/crosscolor.js
+++ test/crosscolor.js
@@ -637,7 +637,7 @@ function help(){
 		popWords(moreWords[moreCount]);
 		moreCount++;
 		helpFlag=false;
-		$('.centerbody').attr('style','-webkit-filter: blur(3px);');
+		$('.centerbody').attr('style','-webkit-filter:  grayscale(0.7);');
 		var offset=$('.green').offset();
 		var startHelp = $('<div class="bounceside" ><font style="background-color:transparent;font-size:30px" >👈</font><font style="border-radius:6px;border:1px solid dimgray;padding:2px;background: linear-gradient(#EEEEEE, #DDFF96,#DDFF96);" >Start</font></div>');
 				//alert(offset);
