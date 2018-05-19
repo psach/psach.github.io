0a061437e75c9c4ad864c141ffd9c426597eb0b5
psach <37810543+psach@users.noreply.github.com>
2018-05-19 21:51:08 +0530

    Update crosscolor.js

--- test/crosscolor.js
+++ test/crosscolor.js
@@ -569,7 +569,8 @@ function showLevel(){
 	
 	$(".wrapperContainer > .wrapper").remove();
 	$(".wrapperContainer")
-	.append('<div class="wrapper" style="-webkit-animation-delay:2s" ><table width=100% ><tr><td></td><td class="score" align="left" >'+(totalLevels)+'</td><td width="85%"></td></tr></table></div>');
+	.append($('<div class="wrapper" style="-webkit-animation-delay:0s" ><table width=100% ><tr><td></td><td class="score" align="left" >'+
+		  (totalLevels)+'</td><td width="85%"></td></tr></table></div>'));
 	
 	
 	helpFlag=false;
